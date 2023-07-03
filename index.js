46886772b81d4b86b0383bab3793b8fbf7d136de
Yasmineboulila <132713541+Yasmineboulila@users.noreply.github.com>
2023-07-03 12:31:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+
 var btnsPlus = document.querySelectorAll(".btn-plus");
 var btnsMinus = document.querySelectorAll(".btn-minus");
 var counts = document.querySelectorAll(".count");
-var like = document.getElementsByClassName("bi-star");
+var stars = document.getElementsByClassName("bi-star");
 var trash = document.getElementsByClassName("bi-trash-fill");
 var prix = document.getElementsByClassName("price");
 var total = document.querySelector(".total-price");
 
 
 function total_price(){
-    let sum = 0
+    let sum = 0;
     for (let i=0; i<btnsPlus.length;i++){
-        sum = sum + prix[i].innerText * counts[i].innerText
+        sum = sum + Number(prix[i].innerText) * Number(counts[i].innerText);
     }
-    total.innerText = sum
+    total.innerText = sum;
 }
 
 
@@ -20,7 +21,7 @@ for (let i = 0; i <= btnsPlus.length - 1; i++) {
   btnsPlus[i].addEventListener("click", function () {
     //   count.innerText = Number(count.innerText) + 1;
     counts[i].innerText++;
-    total_price()
+    total_price();
   });
 
   btnsMinus[i].addEventListener("click", function () {
@@ -28,7 +29,15 @@ for (let i = 0; i <= btnsPlus.length - 1; i++) {
       counts[i].innerText--;
     }
     if (total.innerText >0)
-            total.innerText -= Number(prix[i].innerText)
-            total_price()
+            total.innerText -= Number(prix[i].innerText);
+            total_price();
   });
+}
+ 
+// stars
+for (let i=0;i<stars.length; i++){
+    stars[i].addEventListener("click",function() {
+        if (stars[i].style.color !== "gold") stars[i].style.color="gold" ;
+        else stars[i].style.color ="black";
+    })
 }
